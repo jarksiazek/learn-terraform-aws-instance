6f4e490d4952332be968c2d2d749f73cc7efdf52
jarksiazek <jksiazek@pgs-soft.com>
2020-11-02 10:18:41 +0100

    adding listener rule to ALB - working

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -1,9 +1,12 @@
-resource "aws_lb_target_group" "target_group" {
-  name     = "tg"
+resource "aws_lb_target_group" "target_group_default" {
+  name     = "tg-default"
   target_type = "instance"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  lifecycle {
+    create_before_destroy = true
+  }
   health_check {
     protocol            = "HTTP"
     path                = "/"
@@ -15,10 +18,35 @@ resource "aws_lb_target_group" "target_group" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "target_group_attach_web_instances" {
-  count            = length(var.instance_ids)
-  target_group_arn = aws_lb_target_group.target_group.arn
-  target_id        = element(var.instance_ids, count.index)
+resource "aws_lb_target_group" "target_group_target1" {
+  name     = "tg-target1"
+  target_type = "instance"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  lifecycle {
+    create_before_destroy = true
+  }
+  health_check {
+    protocol            = "HTTP"
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 10
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "target_group_default_attach_web_instances" {
+  target_group_arn = aws_lb_target_group.target_group_default.arn
+  target_id        = var.instance_ids[0]
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "target_group_target1_attach_web_instances" {
+  target_group_arn = aws_lb_target_group.target_group_target1.arn
+  target_id        = var.instance_ids[1]
   port             = 80
 }
 
@@ -36,6 +64,21 @@ resource "aws_lb_listener" "front_end" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.target_group.arn
+    target_group_arn = aws_lb_target_group.target_group_default.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "target_rule" {
+  listener_arn  = aws_lb_listener.front_end.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group_target1.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/target/*"]
+    }
   }
 }
